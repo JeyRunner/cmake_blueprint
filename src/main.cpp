1df33d42aca963e568a08c01dba58b463c6601af
JeyRunner <JeyRunner@users.noreply.github.com>
2016-12-27 17:54:50 +0100

    correct bin dir

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,13 @@
 //
 #include <stdio.h>
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main(int argCount, char *args[])
 {
   printf("program arguments (%i) of %s",
          argCount, args[0]);
+
+  exit(42);
 }
