098858779fefaa5e6a0e86b163dfe68061db5306
JeyRunner <JeyRunner@users.noreply.github.com>
2016-12-27 16:57:51 +0100

    init

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by joshua on 27.12.16.
+//
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(int argCount, char *args[])
+{
+  printf("program arguments (%i) of %s",
+         argCount, args[0]);
+}
